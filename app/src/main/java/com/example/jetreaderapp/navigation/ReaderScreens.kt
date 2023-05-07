eea1bae29fdf7f29481a57b5c1715c68addf756d
Yury <matatov1989@gmail.com>
2023-05-07 19:54:51 +0300

    structuring the project and adding all screens

---
+++ app/src/main/java/com/example/jetreaderapp/navigation/ReaderScreens.kt
@@ -0,0 +1,4 @@
+package com.example.jetreaderapp.navigation
+
+enum class ReaderScreens {
+}
---
+++ app/src/main/java/com/example/jetreaderapp/screens/stats/ReaderStatsScreen.kt
@@ -0,0 +1,8 @@
+package com.example.jetreaderapp.screens.stats
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ReaderStatsScreen() {
+
+}
