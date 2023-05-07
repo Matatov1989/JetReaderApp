c037f8536f367c19b27b648d929ee5afaeff842f
Yury <matatov1989@gmail.com>
2023-05-07 19:42:28 +0300

    setting up di with hilt

--- app/src/main/java/com/example/jetreaderapp/MainActivity.kt
+++ app/src/main/java/com/example/jetreaderapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.jetreaderapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,17 +12,38 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetreaderapp.ui.theme.JetReaderAppTheme
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetReaderAppTheme {
+
+
+                val db = FirebaseFirestore.getInstance()
+                val user: MutableMap<String, Any> = HashMap()
+                user["firstName"] = "Joe"
+                user["lastName"] = "James"
+
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+
+                    db.collection("Users")
+                        .add(user)
+                        .addOnSuccessListener {
+                            Log.d("FB", "onCreate: ${it.id}")
+                        }
+                        .addOnFailureListener {
+                            Log.d("FB", "onCreate: ${it}")
+                        }
+
                     Greeting("Android")
                 }
             }
