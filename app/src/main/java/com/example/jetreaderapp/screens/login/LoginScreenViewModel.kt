6652f973503f6d7fdc39dbbca5eb4df20fd65464
Yury <matatov1989@gmail.com>
2023-05-13 17:22:29 +0300

    creating the user model

--- app/src/main/java/com/example/jetreaderapp/screens/login/LoginScreenViewModel.kt
+++ app/src/main/java/com/example/jetreaderapp/screens/login/LoginScreenViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.jetreaderapp.model.MUser
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -41,10 +42,15 @@ class LoginScreenViewModel : ViewModel() {
 
     private fun createUser(displayName: String?) {
         val userId = auth.currentUser?.uid
-        val user = mutableMapOf<String, Any>()
-        user["user_id"] = userId.toString()
-        user["display_name"] = displayName.toString()
-
+        val user =
+            MUser(
+                userId = userId.toString(),
+                displayName = displayName.toString(),
+                avatar = "",
+                quote = "Life is greate",
+                profession = "Android Developer",
+                id = null
+            ).toMap()
         FirebaseFirestore.getInstance().collection("users").add(user)
     }
 
