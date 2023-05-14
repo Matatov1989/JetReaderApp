6c2386f4310f9232da76d32130a83b2adbe2da95
Yury <matatov1989@gmail.com>
2023-05-14 19:48:00 +0300

    adding the top reading section

--- app/src/main/java/com/example/jetreaderapp/screens/home/ReaderHomeScreen.kt
+++ app/src/main/java/com/example/jetreaderapp/screens/home/ReaderHomeScreen.kt
@@ -2,10 +2,7 @@ package com.example.jetreaderapp.screens.home
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -18,15 +15,18 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.jetreaderapp.model.MBook
 import com.example.jetreaderapp.navigation.ReaderScreens
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.ktx.Firebase
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
@@ -39,9 +39,21 @@ fun HomeScreen(navController: NavController) {
         }
     }) {
         Surface(modifier = Modifier.fillMaxSize()) {
-
+            HomeContent(navController)
         }
     }
+}
+
+@Preview
+@Composable
+fun HomeContent(navController: NavController = NavController(LocalContext.current)) {
+    Column(Modifier.padding(2.dp), verticalArrangement = Arrangement.SpaceEvenly) {
+        Row(modifier = Modifier.align(alignment = Alignment.Start)) {
+            TitleSection(label = "You reading \n activity right now...")
+            
+        }
+    }
+    
 }
 
 @Composable
@@ -71,9 +83,35 @@ fun ReaderAppBar(title: String, showProfile: Boolean = true, navController: NavC
                 navController.navigate(ReaderScreens.LoginScreen.name)
             }
         }) {
-            Icon(imageVector = Icons.Filled.Logout, contentDescription = "Logout")
+            Icon(
+                imageVector = Icons.Filled.Logout,
+                contentDescription = "Logout",
+//                tint = Color.Green.copy(alpha = 0.4f)
+            )
         }
     }, backgroundColor = Color.Transparent, elevation = 0.dp)
+}
+
+@Composable
+fun TitleSection(modifier: Modifier = Modifier, label: String) {
+    Surface(modifier = modifier.padding(start = 5.dp, top = 1.dp)) {
+        Column {
+            Text(
+                text = label,
+                fontSize = 19.sp,
+                fontStyle = FontStyle.Normal,
+                textAlign = TextAlign.Center
+            )
+
+        }
+
+    }
+
+}
+
+@Composable
+fun ReadingRightNowArea(books: List<MBook>, navController: NavController) {
+
 }
 
 @Composable
