59340801f9a64ca88474028bf8a1214f6bedfd15
Yury <matatov1989@gmail.com>
2023-05-17 21:24:01 +0300

    search screen ui setup finished

--- app/src/main/java/com/example/jetreaderapp/screens/search/ReaderBookSearchScreen.kt
+++ app/src/main/java/com/example/jetreaderapp/screens/search/ReaderBookSearchScreen.kt
@@ -1,28 +1,33 @@
 package com.example.jetreaderapp.screens.search
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
 import com.example.jetreaderapp.components.InputField
 import com.example.jetreaderapp.components.ReaderAppBar
-import com.example.jetreaderapp.navigation.ReaderScreens
+import com.example.jetreaderapp.model.MBook
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Preview
@@ -43,12 +48,71 @@ fun SearchScreen(navController: NavController = NavController(LocalContext.curre
     ) {
         Surface() {
             Column {
-                SearchForm(modifier = Modifier.fillMaxWidth().padding(16.dp))
+                SearchForm(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                ) {
+
+                }
+                Spacer(modifier = Modifier.height(13.dp))
+                BookList(navController)
+            }
+        }
+    }
+}
+
+@Composable
+fun BookList(navController: NavController) {
+    val listBooks = listOf(
+        MBook(id = "dfdf", title = "Hello Again", author = "All", notes = null),
+        MBook(id = "kl", title = "Again", author = "All", notes = null),
+        MBook(id = "hkji", title = "Hello lol", author = "All gfhfgh", notes = null),
+        MBook(id = "ws", title = "fff Again", author = "All", notes = null),
+        MBook(id = "ju", title = "Hello g", author = "All", notes = null),
+    )
+
+    LazyColumn(modifier = Modifier.fillMaxSize(), contentPadding = PaddingValues(16.dp)) {
+        items(items = listBooks) { book ->
+            BookRow(book, navController)
+
+        }
+    }
+}
+
+@Composable
+fun BookRow(book: MBook, navController: NavController) {
+    Card(
+        modifier = Modifier
+            .clickable { }
+            .fillMaxWidth()
+            .height(100.dp)
+            .padding(3.dp),
+        shape = RectangleShape,
+        elevation = 7.dp
+    ) {
+        Row(modifier = Modifier.padding(5.dp), verticalAlignment = Alignment.Top) {
+            val imageUrl =
+                "http://books.google.com/books/content?id=M7ngCAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+            Image(
+                painter = rememberImagePainter(data = imageUrl),
+                contentDescription = "book image",
+                modifier = Modifier.width(80.dp).fillMaxHeight().padding(end = 4.dp)
+            )
+            Column() {
+                Text(text = book.title.toString(), overflow = TextOverflow.Ellipsis)
+                Text(
+                    text = "Author: ${book.author}",
+                    overflow = TextOverflow.Clip,
+                    style = MaterialTheme.typography.caption
+                )
+
             }
 
         }
 
     }
+
 }
 
 @OptIn(ExperimentalComposeUiApi::class)
